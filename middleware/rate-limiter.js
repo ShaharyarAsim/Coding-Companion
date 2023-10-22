b03c857e0ebbfe5b9e55ab3b53d1c83efbc3f60f
Shaharyar <91311749+ShaharyarAsim@users.noreply.github.com>
2023-10-22 18:54:18 +1100

    Added latest frontend build.
    Fixed code api rate limit.

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -13,8 +13,8 @@ const userAPIRateLimiter = rateLimit({
 });
 
 const codeAPIRateLimiter = rateLimit({
-  windowMs: 5 * 1000, // Time in ms
-  max: 2, // Max requests allowed per time limit
+  windowMs: 3 * 1000, // Time in ms
+  max: 3, // Max requests allowed per time limit
   statusCode: 200,
   keyGenerator: (req, res) => {
     return req.clientIp; // IP address from requestIp.mw()
